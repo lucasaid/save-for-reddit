b01d90ab1bc914db8c95b0a538451fd51b6e6732
Chris Lucas <chris@hhd.com.au>
2019-03-08 13:32:24 +1100

    Stying and hide/show tag input

--- src/server/src/components/reddit-list/post.js
+++ src/server/src/components/reddit-list/post.js
@@ -1,46 +1,93 @@
-import React, { memo } from "react"
+import React from "react"
 import Truncate from "react-truncate"
 import styles from "./reddit-list.module.scss"
 
-const Post = memo(({ post, removePost, addTag }) => {
-  const tags = () => {
-    return post.tags.map((tag, index) => <div key={index}>{tag}</div>)
-  }
-  const removePostThis = e => {
-    removePost(post.id)
-  }
-  const addTagThis = e => {
-    if (e.key === "Enter") {
-      addTag(post.id, e.target.value)
-      e.target.value = ""
+class Post extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tagOpen: false,
     }
   }
-  return (
-    <div className={`${styles.post}`}>
-      <a
-        href={post.url ? post.url : `https://reddit.com/${post.permalink}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2>
-          <Truncate lines={2}>
-            {!post.parent_id && post.title !== ""
-              ? post.title
-              : `Reddit Comment on - ${post.link_title}`}
-          </Truncate>
-        </h2>
-        <p>
-          <Truncate lines={6}>
-            {post.selftext_html && (
-              <span dangerouslySetInnerHTML={{ __html: post.selftext_html }} />
-            )}
-          </Truncate>
-        </p>
-      </a>
-      <input type="text" className={styles.taginput} onKeyPress={addTagThis} />
-      {tags()}
-    </div>
-  )
-})
+  tags = () => {
+    return this.props.post.tags.map((tag, index) => (
+      <div className={styles.tag} key={index}>
+        {tag}
+      </div>
+    ))
+  }
+  // const removePostThis = e => {
+  //   removePost(post.id)
+  // }
+  addTagThis = e => {
+    if (e.key === "Enter") {
+      this.props.addTag(this.props.post.id, e.target.value)
+      e.target.value = ""
+      this.setState({
+        tagOpen: false,
+      })
+    }
+  }
+  showTagInput = e => {
+    e.preventDefault()
+    this.setState({
+      tagOpen: !this.state.tagOpen,
+    })
+  }
+  render() {
+    return (
+      <div className={`${styles.post}`}>
+        <a
+          href={
+            this.props.post.url
+              ? this.props.post.url
+              : `https://reddit.com/${this.props.post.permalink}`
+          }
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            <Truncate lines={2}>
+              {!this.props.post.parent_id && this.props.post.title !== ""
+                ? this.props.post.title
+                : `Reddit Comment on - ${this.props.post.link_title}`}
+            </Truncate>
+          </h2>
+          <p>
+            <Truncate lines={6}>
+              {this.props.post.selftext_html && (
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: this.props.post.selftext_html,
+                  }}
+                />
+              )}
+            </Truncate>
+          </p>
+        </a>
+        {this.state.tagOpen && (
+          <div className={styles.taginputwrapper}>
+            <span>Add Tag</span>
+            <input
+              type="text"
+              className={styles.taginput}
+              onKeyPress={this.addTagThis}
+            />
+          </div>
+        )}
+        <div className={styles.tags}>
+          {this.tags()}
+          <a
+            href="#showtaginput"
+            onClick={this.showTagInput}
+            className={styles.addtag}
+          >
+            {this.state.tagOpen ? `-` : `+`}
+          </a>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Post
---
+++ src/server/src/components/reddit-list/post_nomemo.js
@@ -0,0 +1,47 @@
+import React from "react"
+import Truncate from "react-truncate"
+import styles from "./reddit-list.module.scss"
+
+const Post = ({ post, removePost, addTag }) => {
+  const tags = () => {
+    return post.tags.map((tag, index) => <div key={index}>{tag}</div>)
+  }
+  const removePostThis = e => {
+    removePost(post.id)
+  }
+  const addTagThis = e => {
+    if (e.key === "Enter") {
+      addTag(post.id, e.target.value)
+      e.target.value = ""
+    }
+  }
+  return (
+    <div className={`${styles.post}`}>
+      <a
+        href={post.url ? post.url : `https://reddit.com/${post.permalink}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        onClick={removePostThis}
+      >
+        <h2>
+          <Truncate lines={2}>
+            {!post.parent_id && post.title !== ""
+              ? post.title
+              : `Reddit Comment on - ${post.link_title}`}
+          </Truncate>
+        </h2>
+        <p>
+          <Truncate lines={6}>
+            {post.selftext_html && (
+              <span dangerouslySetInnerHTML={{ __html: post.selftext_html }} />
+            )}
+          </Truncate>
+        </p>
+      </a>
+      <input type="text" className={styles.taginput} onKeyPress={addTagThis} />
+      {tags()}
+    </div>
+  )
+}
+
+export default Post
